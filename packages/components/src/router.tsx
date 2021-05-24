ea2d42c0319fa9d0e376474ce9e28375fa60d73c
Agustin Alejandro Vazquez Ballan <aguss.vazquez@gmail.com>
2021-05-24 00:35:17 -0300

    Added nested navigators

--- packages/components/src/router.tsx
+++ packages/components/src/router.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import HomeScreen from './screens/home.screen'
 
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const NAV_OPTIONS_DEFAULT = {
   headerTintColor: 'black',
@@ -17,13 +20,21 @@ const NAV_OPTIONS_DEFAULT = {
   },
 }
 
+const HomeStack = () => {
+  return (
+    <Stack.Navigator screenOptions={NAV_OPTIONS_DEFAULT}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home2" component={HomeScreen} />
+    </Stack.Navigator>
+  )
+}
+
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={NAV_OPTIONS_DEFAULT}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Home2" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="TabHome" component={HomeStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
